ee1c33ecbd929cb77b33cf94278dfec45a17d931
MayNiklas <info@niklas-steffen.de>
2020-11-09 21:06:12 +0100

    ↗️ Added entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Set groups and permissions
+groupadd python -g ${GID:-1000}
+useradd -u ${UID:-1000} -g ${GID:-1000} python
+chown -R  python:python /app
+
+su python -c 'python3 in-stock.py'
