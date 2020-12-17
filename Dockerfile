585b9ec71ed0b1446e8bbf36f2e8f9fd5d106c53
EinfachAleks <aleks@einfach-aleks.com>
2020-12-17 21:52:45 +0100

    feat(app): replace request with Selenium Library

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,26 @@
 ARG ARCH=
 FROM ${ARCH}python:3.8
 
-RUN apt-get update && apt-get -y upgrade && apt-get install -y \
-    python3-pip
+# make a full upgrade
+RUN apt-get update && apt-get -y upgrade
 
+# install project dependencies
+RUN apt-get install -y python3-pip unzip
+
+# install Google Chrome
+RUN curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
+RUN echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+RUN apt-get -y update
+RUN apt-get -y install google-chrome-stable
+
+# Install ChromeDriver.
+RUN LATEST_VERSION=$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE) && \
+ wget -N https://chromedriver.storage.googleapis.com/$LATEST_VERSION/chromedriver_linux64.zip -P ~/
+RUN unzip ~/chromedriver_linux64.zip -d ~/
+RUN rm ~/chromedriver_linux64.zip
+RUN mv -f ~/chromedriver /usr/local/bin/chromedriver
+RUN chown root:root /usr/local/bin/chromedriver
+RUN chmod 0755 /usr/local/bin/chromedriver
 
 VOLUME /app/
 VOLUME /app/data/
--- src/in-stock.py
+++ src/in-stock.py
@@ -1,38 +1,41 @@
-import os
-import time
 import atexit
-import logging
-import telegram
-import requests
 import datetime
-from time import sleep
+import logging
+import os
 from random import randint
-from telegram import Update
+from time import sleep
+
+import telegram
 from fake_headers import Headers
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from selenium import webdriver
+from telegram import Update
+from telegram.ext import CallbackContext, CommandHandler, Updater
 
 import csv_utils
 
-API_Key = os.environ['API_Key']
-Product = os.environ['Product']
-Product_URL = os.environ['Product_URL']
-UnavailableText = os.environ['UnavailableText']
+# get Configuration from docker-compose.yml file
+apiKey = os.environ['API_Key']
+product = os.environ['Product']
+productUrl = os.environ['Product_URL']
+unavailabilityMessage = os.environ['UnavailableText']
 
-### You won't receive notifications by using /start at this point!
-### Feature is in work!
-def notify(update: Update, context: CallbackContext) -> None:
+# define global variables
+chromedriverPath = 'chromedriver'
+availability = False
+
+# Telegram Command /start
+def startCommand(update: Update, context: CallbackContext) -> None:
     chat_ids = update.message.chat_id
     update.message.reply_text(f"Bot has started!\nYour message ID: {chat_ids}\nYou will receive notifications in the future!")
     chat = writer.add(update.message.chat)
-    
 
-# init telegram
-bot = telegram.Bot(token=API_Key)
+# init Telegram-Bot
+bot = telegram.Bot(token=apiKey)
 
-#gets the read / write object
+# gets the read / write object
 writer = csv_utils.Writer()
 
-#makes sure the list is written before shuting down
+# makes sure the list is written before shuting down
 @atexit.register
 def goodbye():
     writer.write()
@@ -43,34 +46,44 @@ logging.basicConfig(
 	format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 logger = logging.getLogger(__name__)
-updater = Updater(API_Key, use_context=True)
-
+updater = Updater(apiKey, use_context=True)
 dispatcher = updater.dispatcher
-
-dispatcher.add_handler(CommandHandler("start", notify))
+dispatcher.add_handler(CommandHandler("start", startCommand))
 updater.start_polling()
 
-headers = Headers(
-    browser="chrome",  # Generate only Chrome UA
-    os="win",  # Generate ony Windows platform
-    headers=True  # generate misc headers
-).generate()
-
-
 def dispatch_update(writer: object, text: str) -> None:
-    """Iterates trough the list of chat objects - sending update"""
+    # Iterates trough the list of chat objects - sending update
     for chat in writer.entries:
         bot.send_message(chat_id=chat.id, text=text)
+        
+# check the Availability of the Product
+while availability == False:
 
-while True:
-    response = requests.get(f'{Product_URL}', headers=headers)
-    if not UnavailableText in response.text:
-       #sending update based on chat objects
-       dispatch_update(writer, text=f'{Product} lieferbar! {Product_URL}')
-       #basic logging
-       print(datetime.datetime.now().strftime("%A-%Y-%m-%d %H:%M:%S")+" - Product available!")
-    
-    sleep(randint(30,60))
+    # generade random Windows Google Chrome fake header
+	headers = Headers(
+		browser="chrome",
+		os="win",
+		headers=True
+	).generate()
+
+	# create Selenium Chrome configuration
+	options = webdriver.ChromeOptions()
+	options.add_argument("--headless")
+	options.add_argument('--no-sandbox')
+	options.add_argument('--disable-dev-shm-usage')
+	options.add_argument('--ignore-certificate-errors')
+	options.add_argument("user-agent="+headers["User-Agent"])
+	chrome = webdriver.Chrome(executable_path=chromedriverPath, options=options)
+	chrome.get(productUrl)
+
+	# check if UnavailableText is visible on the page
+	if not chrome.find_elements_by_xpath("//*[contains(text(),'"+unavailabilityMessage+"')]"):
+		dispatch_update(writer, text=f'Jetzt {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} ist das {product} lieferbar! \n {productUrl}')
+		dispatch_update(writer, text=f'Falls du das {product} weiterhin hier {productUrl} beobachten willst, musst du den Docker Deinst neu Starten.')
+		availability = True
+		chrome.quit()
+
+	# wait before make a new request for checking the product
+	sleep(randint(30,60))
 
 updater.idle()
-driver.close()
